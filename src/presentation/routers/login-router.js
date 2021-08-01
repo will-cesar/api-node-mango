afbd7decbf3c3efb89d878a398e3a7a47b1faaba
will-cesar <willcesar010421@gmail.com>
2021-08-01 09:59:57 -0300

    adjustments pre-commit

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,27 +1,27 @@
-const HttpResponse = require('../helpers/http-response');
+const HttpResponse = require('../helpers/http-response')
 
 module.exports = class LoginRouter {
-    constructor(authUseCase) {
-        this.authUseCase = authUseCase
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
+  route (httpRequest) {
+    if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
+      return HttpResponse.serverError()
     }
 
-    route (httpRequest) {
-        if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth)  {
-            return HttpResponse.serverError()
-        }
+    const { email, password } = httpRequest.body
 
-        const { email, password } = httpRequest.body
-
-        if (!email) {
-            return HttpResponse.badRequest('email')
-        }
-
-        if (!password) {
-            return HttpResponse.badRequest('password')
-        }
-
-        this.authUseCase.auth(email, password)
-
-        return HttpResponse.unauthorizedError()
+    if (!email) {
+      return HttpResponse.badRequest('email')
     }
+
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+
+    this.authUseCase.auth(email, password)
+
+    return HttpResponse.unauthorizedError()
+  }
 }
