1fccbc6fdb2a74b4e7b3f4b9ec6cd11ec8dd7cf8
will-cesar <willcesar010421@gmail.com>
2021-08-02 23:05:09 -0300

    ensure AuthUseCase throws MissingParamError if no password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,18 +1,39 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
+    }
+
+    if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
 
 describe('Auth UseCase', () => {
   test('Should throw if no email is provided', () => {
+    /*
+      - Nesse teste é esperado o retorno de um erro do tipo MissingParamError
+      quando não passar um email na chamada da classe AuthUseCase()
+      - const promise => está sendo retornada uma promise pois o método auth()
+      é assíncrono, e a variável "promise" não está utilizando await para esperar o retorno
+    */
     const sut = new AuthUseCase()
     const promise = sut.auth()
 
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should throw if no password is provided', () => {
+    /*
+      - Nesse teste é esperado o retorno de um erro do tipo MissingParamError
+      quando não passar uma senha na chamada da classe AuthUseCase()
+    */
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com')
+
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
