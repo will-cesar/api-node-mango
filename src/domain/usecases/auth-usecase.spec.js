343b95e0476e38c079f7a72eae8548b943178ea6
will-cesar <willcesar010421@gmail.com>
2021-08-03 22:15:44 -0300

    ensure AuthUseCase returns null if an invalid password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -9,10 +9,13 @@ const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+
+      return this.user
     }
   }
 
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {}
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
   return { sut, loadUserByEmailRepositorySpy }
 }
@@ -82,7 +85,7 @@ describe('Auth UseCase', () => {
     expect(promise).rejects.toThrow()
   })
 
-  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+  test('Should return null if an invalid email is provided', async () => {
     /*
       - Teste para garantir que a classe AuthUseCase vai retornar um valor null
       caso o repositório, LoadUserByEmailRepository, retorne null
@@ -90,8 +93,23 @@ describe('Auth UseCase', () => {
       null pela classe
     */
 
-    const { sut } = makeSut()
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.user = null
     const accessToken = await sut.auth('invalid_email@email.com', 'any_password')
+
+    expect(accessToken).toBeNull()
+  })
+
+  test('Should return null if an invalid password is provided', async () => {
+    /*
+      - Teste para garantir que a classe AuthUseCase vai retornar um valor null
+      caso o repositório, LoadUserByEmailRepository, retorne null
+      - Ou seja, caso o password seja inválido, será retornado um valor
+      null pela classe
+    */
+
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('valid_email@email.com', 'invalid_password')
 
     expect(accessToken).toBeNull()
   })
