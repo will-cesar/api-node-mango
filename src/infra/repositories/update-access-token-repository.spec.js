9c97d089f1875b426e01bbb563a3e16b44367a50
will-cesar <willcesar010421@gmail.com>
2021-09-04 20:54:45 -0300

    move sut creation to a factory helper method

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -20,6 +20,12 @@ class UpdateAccessTokenRepository {
   }
 }
 
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new UpdateAccessTokenRepository(userModel)
+  return { userModel, sut }
+}
+
 describe('UpdateAccessTokenRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -35,8 +41,7 @@ describe('UpdateAccessTokenRepository', () => {
   })
 
   test('Should update the user with the given accessToken', async () => {
-    const userModel = db.collection('users')
-    const sut = new UpdateAccessTokenRepository(userModel)
+    const { sut, userModel } = makeSut()
     const fakeUser = await userModel.insertOne({
       email: 'valid_email@email.com',
       name: 'any_name',
