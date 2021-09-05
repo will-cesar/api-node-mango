b9d9c454c8c12b9da1a6841ca010f3483afa4c01
will-cesar <willcesar010421@gmail.com>
2021-09-04 22:27:19 -0300

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,4 +1,12 @@
 module.exports = (app) => {
   // remove uma propriedade padrão do header do express
   app.disable('x-powered-by')
+
+  // libera o CORS
+  app.use((req, res, next) => {
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,6 +3,10 @@ const app = require('./app')
 
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
+    /*
+      Teste para saber se a propriedade "x-powered-by" foi removida
+    */
+
     // cria uma rota apenas para testes
     app.get('/test_x_powered_by', (req, res) => {
       res.send('')
@@ -10,5 +14,22 @@ describe('App Setup', () => {
 
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    /*
+      Teste para saber se a app está permitindo requisições
+      de outros domínios (CORS)
+    */
+
+    // cria uma rota apenas para testes
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
